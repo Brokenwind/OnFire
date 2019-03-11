48a04c663648fc53aa3be496e0e6f0fcdfb9bc98
Brokenwind <wangkun6536@163.com>
2019-03-12 07:06:33 +0800

    add: Added one media problem

---
+++ src/com/broken/cate/leet/media/PerfectSquares.java
@@ -0,0 +1,22 @@
+package com.broken.cate.leet.media;
+
+public class PerfectSquares {
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j <= i; j++) {
+                if (i - j * j < 0) {
+                    break;
+                }
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numSquares(3));
+    }
+}
